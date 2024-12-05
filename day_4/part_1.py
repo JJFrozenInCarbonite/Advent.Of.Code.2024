be550fed9a20067c1baaf471bf0d7df766039c9c
JJ Hoffmann <JoHoffmann@geico.com>
2024-12-05 06:54:28 -0800

    day_4 completed

--- day_4/part_1.py
+++ day_4/part_1.py
@@ -1,65 +1,28 @@
 source = 'day_4\\input.txt'
 
-def vertStrings(data: list, h, w):
-    strings = []
-    
-    for i in range(0, w):
-        string = ''
-        for j in range(0, h):
-            string += data[j][i]
-        strings.append(string)
-    return strings
-        
-def xStrings(data: list, h, w):
-    strings = []
-
-    for i in range(0, h):
-        string = ''
-        for j in range(0, i + 1):
-            a = h - i - j - 1
-            b = w - j - 1
-            #print(f"({a}, {b}), ", end='')
-            #print(data[i - j][j], end='')
-            #print(10 - i, 10 - j, end=' ')
-            print(data[b][a], end='')
-            #print(data[9 - j][9 - i], end='')
-        print()
-
-            
-
-def new(data: list, h, w):
-    strings = []
-    for i in range(h - 1, -1, -1):
-        for j in range(0, w - 1):
-            a = i
-            b = j
-            print(f'({a}, {b})', end = '')
-        print()
-
-
-
-
-if __name__ == '__main__':
-    data = open(source).read().split('\n')
-    height = len(data)
-    width = len(data[0])
-
-    
-
-    strings = data
-    strings.extend(vertStrings(data, height, width))
-    #xStrings(data, height, width)
-    new(data, height, width)
-
-    print(strings)
-
-    #print(strings)
-    #print(get_vertical_strings(rows))
-    
-
-
-    
-
-
-# row start: 3, 7 | 3, len(row) - 3
-# column start: 3, 7 | 3, len(column) - 3
+data = open(source).read().split('\n')
+xmas = []
+for i, row in enumerate(data):
+    for j, char in enumerate(row):
+        if j <= len(row) - 4:
+            xmas.append(row[j:j+4])
+        if i <= len(data) - 4:
+            string = ''
+            for k in range(0, 4):
+                string += data[i + k][j]
+            xmas.append(string)
+        if i <= len(data) - 4 and j <= len(row) - 4:
+            string = ''
+            for k in range(0, 4):
+                string += data[i + k][j + k]
+            xmas.append(string)
+        if i >= 3 and j <= len(row) - 4:
+            string = ''
+            for k in range(0, 4):
+                string += data[i - k][j + k]
+            xmas.append(string)
+count = 0
+for x in xmas:
+    if x == 'XMAS' or x == 'SAMX':
+        count += 1
+print(count)
--- day_4/part_2.py
+++ day_4/part_2.py
@@ -1,10 +1,13 @@
-import re
+source = 'day_4\\input.txt'
 
-source = 'day_3\\input.txt'
-
-data = open(source).read()
-data = data.split('do()')
-data = ''.join([d.split("don't()")[0] for d in data])
-data = sum([int(mult[4:-1].split(',')[0]) * int(mult[4:-1].split(',')[1]) for mult in re.findall('mul\\(\\d*,\\d*\\)', data)])
-
-print(data)
+data = open(source).read().split('\n')
+count = 0
+for i, row in enumerate(data):
+    for j, char in enumerate(row):
+        if char == 'A':
+            if i >= 1 and i <= len(data) - 2 and j >= 1 and j <= len(row) - 2:
+                string1 = data[i - 1][j - 1] + data[i + 1][j + 1]
+                string2 = data[i - 1][j + 1] + data[i + 1][j - 1]
+                if (string1 == 'MS' or string1 == 'SM') and (string2 == 'MS' or string2 == 'SM'):
+                    count += 1
+print(count)
